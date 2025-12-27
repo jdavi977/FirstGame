ea145111b5edb4f72904132d8c6e2ddb303b9fc4
Jawne3 <johnlowendavid@yahoo.ca>
2025-12-27 12:36:14 -0700

    Finished basic Player class, created MonsterTemplates, working on Monster class

---
+++ src/shared/Templates/MonsterTemplates.lua
@@ -0,0 +1,12 @@
+return {
+    Slime = {
+        Health = 30,
+        AttackPoints = 5,
+        ExperiencePoints = 10,
+    },
+    Goblin = {
+        Health = 50,
+        AttackPoints = 15,
+        ExperiencePoints = 25,
+    },
+}
